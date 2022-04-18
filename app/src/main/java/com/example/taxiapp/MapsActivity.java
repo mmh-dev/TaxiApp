0daa69cd77e77b54a53da4839e77d0db8874f55d
mmh-dev <murod.hodjaev@gmail.com>
2022-04-18 20:53:09 +0300

    1

--- app/src/main/java/com/example/taxiapp/MapsActivity.java
+++ app/src/main/java/com/example/taxiapp/MapsActivity.java
@@ -1,10 +1,5 @@
 package com.example.taxiapp;
 
-import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
@@ -13,8 +8,6 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +15,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -35,7 +32,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
 import java.util.List;
@@ -49,132 +45,132 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    Button getTaxi;
-    TextView UserAddress;
-    ImageView exit_btn;
-    SharedPreferences preferences;
-    String address;
+	private GoogleMap mMap;
+	Button getTaxi;
+	TextView UserAddress;
+	ImageView exit_btn;
+	SharedPreferences preferences;
+	String address;
 
-    FusedLocationProviderClient fusedLocationProviderClient;
-    LocationRequest locationRequest;
-    LocationCallback locationCallback;
-    Location userLocation;
+	FusedLocationProviderClient fusedLocationProviderClient;
+	LocationRequest locationRequest;
+	LocationCallback locationCallback;
+	Location userLocation;
 
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maps);
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        buildLocationRequest();
+		fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+		buildLocationRequest();
 
 
-        getTaxi = findViewById(R.id.getTaxi);
-        UserAddress = findViewById(R.id.address);
-        exit_btn = findViewById(R.id.exit_btn);
+		getTaxi = findViewById(R.id.getTaxi);
+		UserAddress = findViewById(R.id.address);
+		exit_btn = findViewById(R.id.exit_btn);
 
-        preferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
-        exit_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("token", "");
-                editor.apply();
-                Toast.makeText(MapsActivity.this, "User is logged out!", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MapsActivity.this, Register.class));
-            }
-        });
+		preferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+		exit_btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				SharedPreferences.Editor editor = preferences.edit();
+				editor.putString("token", "");
+				editor.apply();
+				Toast.makeText(MapsActivity.this, "User is logged out!", Toast.LENGTH_SHORT).show();
+				startActivity(new Intent(MapsActivity.this, Register.class));
+			}
+		});
 
-        userLocation = new Location("");
-        getTaxi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Order order = new Order(new GeoPoint("GeoPoint", userLocation.getLatitude(), userLocation.getLongitude()), getIntent().getStringExtra("username"), address, getIntent().getStringExtra("phone"), false);
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://parseapi.back4app.com")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-                Api api = retrofit.create(Api.class);
-                Call<ApiResponseUserSignUp> call = api.createOrder(order);
-                call.enqueue(new Callback<ApiResponseUserSignUp>() {
+		userLocation = new Location("");
+		getTaxi.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Order order = new Order(new GeoPoint("GeoPoint", userLocation.getLatitude(), userLocation.getLongitude()), getIntent().getStringExtra("username"), address, getIntent().getStringExtra("phone"), false);
+				Retrofit retrofit = new Retrofit.Builder()
+						.baseUrl("https://parseapi.back4app.com")
+						.addConverterFactory(GsonConverterFactory.create())
+						.build();
+				Api api = retrofit.create(Api.class);
+				Call<ApiResponseUserSignUp> call = api.createOrder(order);
+				call.enqueue(new Callback<ApiResponseUserSignUp>() {
 
-                    @Override
-                    public void onResponse(Call<ApiResponseUserSignUp> call, Response<ApiResponseUserSignUp> response) {
-                        if (response.isSuccessful()){
-                            Toast.makeText(MapsActivity.this, "Order is registered!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
+					@Override
+					public void onResponse(Call<ApiResponseUserSignUp> call, Response<ApiResponseUserSignUp> response) {
+						if (response.isSuccessful()) {
+							Toast.makeText(MapsActivity.this, "Order is registered!", Toast.LENGTH_SHORT).show();
+						}
+					}
 
-                    @Override
-                    public void onFailure(Call<ApiResponseUserSignUp> call, Throwable t) {
-                        Log.i("error", t.getMessage());
-                    }
-                });
-            }
-        });
+					@Override
+					public void onFailure(Call<ApiResponseUserSignUp> call, Throwable t) {
+						Log.i("error", t.getMessage());
+					}
+				});
+			}
+		});
 
-    }
+	}
 
-    private void buildLocationRequest(){
-        locationRequest = new LocationRequest();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(100);
-    }
+	private void buildLocationRequest() {
+		locationRequest = new LocationRequest();
+		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		locationRequest.setInterval(100);
+	}
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-            }
-        }
-    }
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+			if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+				fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+			}
+		}
+	}
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		mMap = googleMap;
+		mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
-        }
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+			return;
+		}
 
-        mMap.setMyLocationEnabled(true);
-        locationCallback = new LocationCallback(){
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                for (Location location: locationResult.getLocations()) {
-                    userLocation = location;
-                    mMap.clear();
-                    LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(position).title("Your position")
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
+		mMap.setMyLocationEnabled(true);
+		locationCallback = new LocationCallback() {
+			@Override
+			public void onLocationResult(LocationResult locationResult) {
+				super.onLocationResult(locationResult);
+				for (Location location : locationResult.getLocations()) {
+					userLocation = location;
+					mMap.clear();
+					LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+					mMap.addMarker(new MarkerOptions().position(position).title("Your position")
+							.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+					mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
 
-                    Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
-                    try {
-                        List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        address = addressList.get(0).getAddressLine(0);
-                        UserAddress.setText(address);
+					Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+					try {
+						List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+						address = addressList.get(0).getAddressLine(0);
+						UserAddress.setText(address);
 
-                    } catch (IOException e) {
-                        e.getMessage();
-                    }
-                }
-            }
-        };
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-    }
+					} catch (IOException e) {
+						e.getMessage();
+					}
+				}
+			}
+		};
+		fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+	}
 }
